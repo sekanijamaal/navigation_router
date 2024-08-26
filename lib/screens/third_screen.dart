66de240db1df49f74837619fbebb4db018d42f14
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-08-26 10:54:08 +0000

    Creating various screens for app

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Third Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.check_circle,
-              color: Colors.green,
-              size: 100,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'All Done!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.popUntil(context, (route) => route.isFirst);
-              },
-              child: Text('Back to First Screen'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
